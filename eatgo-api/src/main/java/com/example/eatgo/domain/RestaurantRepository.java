afaaa9e487110cf97a8df1093f404401531e725e
yunsy1103 <yunsy1103@naver.com>
2024-02-07 00:26:19 +0900

    의존성 주입

--- eatgo-api/src/main/java/com/example/eatgo/domain/RestaurantRepository.java
+++ eatgo-api/src/main/java/com/example/eatgo/domain/RestaurantRepository.java
@@ -1,26 +1,9 @@
 package com.example.eatgo.domain;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class RestaurantRepository {
-    //도네임 모델을 만들어서 데이터 및 컨트롤러 정리(중복 제거)
-    //더 많은 가게 관리 가능
-    private List<Restaurant> restaurants = new ArrayList<>();
-    public RestaurantRepository(){
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
-    }
+public interface RestaurantRepository {
+    List<Restaurant> findAll();
 
-
-    public List<Restaurant> findAll(){
-
-        return restaurants;
-    }
-    public Restaurant findById(Long id){
-        return restaurants.stream()
-                .filter(r->r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
+    Restaurant findById(Long id);
 }
--- eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
@@ -2,16 +2,16 @@ package com.example.eatgo.interfaces;
 
 import com.example.eatgo.domain.Restaurant;
 import com.example.eatgo.domain.RestaurantRepository;
+import com.example.eatgo.domain.RestaurantRepositoryImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
-    private RestaurantRepository repository = new RestaurantRepository();
+    private RestaurantRepository repository;
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
 
