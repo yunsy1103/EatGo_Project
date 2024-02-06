895530797756b7ca5907a48a07bc49b7dbe2a988
yunsy1103 <yunsy1103@naver.com>
2024-02-06 18:38:00 +0900

    restaurant 리포지토리 생성

--- eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/example/eatgo/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.example.eatgo.interfaces;
 
 import com.example.eatgo.domain.Restaurant;
+import com.example.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,27 +11,18 @@ import java.util.List;
 
 @RestController
 public class RestaurantController {
+    private RestaurantRepository repository = new RestaurantRepository();
     @GetMapping("/restaurants")
     public List<Restaurant> list(){
-        List<Restaurant> restaurants = new ArrayList<>();
 
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id){ //파라미터 값
-        List<Restaurant> restaurants = new ArrayList<>();//컬렉션에 리스트 만들어서 아이디에 나오는 결과값 구별
-
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber Food","Seoul"));
-        
-        Restaurant restaurant = restaurants.stream()
-                .filter(r->r.getId().equals(id))
-                .findFirst()
-                .get(); //레스토랑에 있는 아이디 찾아서 아이디 값 가져오기
+       Restaurant restaurant = repository.findById(id);//컬렉션에 리스트 만들어서 아이디에 나오는 결과값 구별, repository로 만들어서 수정
 
         return restaurant;
     }
